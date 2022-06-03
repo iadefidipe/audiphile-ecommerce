28764d1e5e40fd056b4b552123191134f5ef4e44
Israel <israeladefidipe@gmail.com>
2022-06-04 00:27:37 +0100

    feat: header component completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,28 +2,27 @@ import React from "react"
 import Image from "next/image"
 import audiophile from "public/assets/audiophile-logo.svg"
 import cart from "public/assets/icon-cart.svg"
-
+import Wrapper from "./shared/Wrapper"
 
 function Header() {
   return (
-    <header className='bg-black m-auto'>
-      
-      <nav className=" flex items-center justify-between gap-7" >
-      <div>
-        <Image src={audiophile} alt='audiophile logo' />
-      </div>
-        <div className="text-white text-nav flex gap-7" >
+    <header className='bg-black mt-8 mx-auto'>
+      <Wrapper >
+        <nav className=" relative flex items-center justify-between gap-7 after:content-[''] after:absolute after:left-0 after:right-0 after:bg-[#979797] after:h-px after:mt-[36px]">
+          <div>
+            <Image src={audiophile} alt='audiophile logo' />
+          </div>
+          <div className='text-white text-nav flex gap-7 font-bold'>
             <a href=''>HOME</a>
             <a href=''>HEADPHONES</a>
             <a href=''>SPEAKERS</a>
             <a href=''>EARPHONES</a>
-        </div>
-        <div>
-            <Image src={cart} alt="" />
-        </div>
-      </nav>
-
-      
+          </div>
+          <div>
+            <Image src={cart} alt='' />
+          </div>
+        </nav>
+      </Wrapper>
     </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,33 +3,45 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        mobile: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
+        mobile: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      maxWidth: {
+        max: "1110px",
       },
       colors: {
-        "pry": "#D87D4A",
+        pry: "#D87D4A",
         "bg-black": "#101010",
         gray: "#F1F1F1",
         "bg-white": "#FAFAFA",
         white: "#FFFFFF",
         black: "#000000",
-        "accent": "#fbaf85",
+        accent: "#fbaf85",
       },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
       },
       fontSize: {
-        h1: '3.5rem',
-        h2: '2.5rem',
-        h3: '2rem',
-        h4: '1.75rem',
-        h5: '1.5rem',
-        h6: '1.125rem',
-        body:'0.9375rem',
-        nav:'0.8125rem'
+        h1: "3.5rem",
+        h2: "2.5rem",
+        h3: "2rem",
+        h4: "1.75rem",
+        h5: "1.5rem",
+        h6: "1.125rem",
+        body: "0.9375rem",
+        nav: "0.8125rem",
+      },
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+      margin: {
+        '5px': '5px',
       }
     },
   },
