425da081f645d73576bdff9f910b425edec75c9b
Israel <israeladefidipe@gmail.com>
2022-06-04 21:48:17 +0100

    fix: update to footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,29 +10,29 @@ import Image from "next/image"
 
 function Footer() {
   return (
-    <footer className='bg-black'>
-      <Wrapper>
-        <Logo />
-        <MenuLinks footer={true} />
-        <p>
-          Audiophile is an all in one stop to fulfill your audio needs. We're a
-          small team of music lovers and sound specialists who are devoted to
-          helping you get the most out of personal audio. Come and visit our
-          demo facility - we’re open 7 days a week.
-        </p>
-
-        <p>Copyright 2021. All Rights Reserved</p>
-
-        <div className="bg-pry">
-          <Link href='/'>
-            <Image src={Facebook} alt='' />
-          </Link>
-          <Link href='/'>
-            <Image src={Twitter} alt='' />
-          </Link>
-          <Link href='/'>
-            <Image src={Instagram} alt='' />
-          </Link>
+    <footer className='bg-black '>
+      <Wrapper  >
+        <div className=" mx-2 py-12 flex flex-col items-center gap-12 md:grid grid-rows-3 grid-cols-2" >
+          <Logo footer={true} />
+          <MenuLinks footer={true} />
+          <p className=" text-50 text-center md:col-span-2 text-left "  >
+            Audiophile is an all in one stop to fulfill your audio needs. We're a
+            small team of music lovers and sound specialists who are devoted to
+            helping you get the most out of personal audio. Come and visit our
+            demo facility - we’re open 7 days a week.
+          </p>
+          <p className="text-50 font-bold" >Copyright 2021. All Rights Reserved</p>
+          <div className="flex items-center gap-4 ">
+            <Link href='/'>
+              <Image src={Facebook} alt='' />
+            </Link>
+            <Link href='/'>
+              <Image src={Twitter} alt='' />
+            </Link>
+            <Link href='/'>
+              <Image src={Instagram} alt='' />
+            </Link>
+          </div>
         </div>
       </Wrapper>
     </footer>
--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import audiophile from "public/assets/audiophile-logo.svg"
 
+interface LogoInterface{
+  footer?: boolean
+}
 
-function Logo() {
+function Logo({footer}: LogoInterface) {
   return (
-    <div>
+    <div className={`${ footer ? "col-span-2" : "" }`} >
     <Image src={audiophile} alt='audiophile logo' />
   </div>
   )
