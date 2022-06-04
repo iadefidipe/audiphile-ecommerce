e3010ad4caa420751d7e6246de3e6f39ecc7594c
Israel Adefidipe <israeladefidipe@gmail.com>
2022-06-04 03:05:34 +0100

    fix: abstracted nav styles using tailwind @apply rule

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,21 @@
 import React from "react"
 import Image from "next/image"
-import audiophile from "public/assets/audiophile-logo.svg"
 import cart from "public/assets/icon-cart.svg"
 import Wrapper from "./shared/Wrapper"
 import Hamburger from "public/assets/icon-hamburger.svg"
+import MenuLinks from "./shared/MenuLinks"
+import Logo from "./shared/Logo"
 
 function Header() {
   return (
     <header className='bg-black p-8 '>
       <Wrapper >
-        <nav className=" relative flex items-center justify-between gap-7 after:content-[''] after:absolute after:left-0 after:right-0 after:bg-[#979797] after:h-px after:mt-[60px]">
+        <nav className=" nav ">
           <div className=" md:hidden" >
             <Image src={Hamburger} alt='audiophile logo' />
           </div>
-          <div>
-            <Image src={audiophile} alt='audiophile logo' />
-          </div>
-          <div className='hidden md:flex text-white text-nav  gap-7 font-bold'>
-            <a href=''>HOME</a>
-            <a href=''>HEADPHONES</a>
-            <a href=''>SPEAKERS</a>
-            <a href=''>EARPHONES</a>
-          </div>
+         <Logo />
+          <MenuLinks   />
           <div>
             <Image src={cart} alt='' />
           </div>
