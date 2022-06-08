ba1c916a287810ed7d1ba6d6a8eb1fa343dcd6ad
Israel Adefidipe <israeladefidipe@gmail.com>
2022-06-08 02:23:40 +0100

    feat: Button components

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -1,11 +1,20 @@
-import React from "react"
+import arrow from "public/assets/shared/desktop/icon-arrow-right.svg"
+import Image from "next/image"
 
-function Button() {
+interface BtnInterface {
+  num: number
+}
+export function BtnPry({num}: BtnInterface) {
+  return <button className={`btn-${num}`}>SEE PRODUCT</button>
+}
+
+export function BtnShop() {
   return (
-    <button className='border-[1px] text-black text-nav border-black px-bx py-by font-bold hover:bg-black hover:text-white'>
-      SEE PRODUCT
+    <button className='btn-shop'>
+      SHOP
+      <span>
+        <Image src={arrow} width={10} height={10} alt='' />
+      </span>
     </button>
   )
 }
-
-export default Button
