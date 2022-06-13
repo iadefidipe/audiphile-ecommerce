3c0c92ef78ee899018eb67e5a3ef24f851b7d930
Israel <israeladefidipe@gmail.com>
2022-06-13 23:14:25 +0100

    feaat: carticon component done

--- components/shared/CartIcon.tsx
+++ components/shared/CartIcon.tsx
@@ -4,7 +4,7 @@ import Cart from "public/assets/shared/desktop/icon-cart.svg"
 function CartIcon() {
   return (
     <div>
-      <Image src={Cart} alt='' />
+      <Image src={Cart} alt='cart icon notification' />
     </div>
   )
 }
