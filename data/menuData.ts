9aae8f2a861f789ef789fab2734ca6e55cf649a1
Israel <israeladefidipe@gmail.com>
2022-06-13 23:16:05 +0100

    feat: changes made to page data

--- data/menuData.ts
+++ data/menuData.ts
@@ -1,26 +1,19 @@
-import audiophile from "public/images/shared/audiophile-logo.svg";
-import cart from "public/images/shared/icon-cart.svg";
-import headphones from "public/images/shared/headphones.png";
-import speakers from "public/images/shared/speakers.png";
-import earphones from "public/images/shared/earphones.png";
+import audiophile from "public/assets/shared/desktop/logo.svg"
+// import cart from "public/images/shared/icon-cart.svg";
+import speakers from "public/assets/shared/desktop/image-category-thumbnail-speakers.png"
+
+import headphones from "public/assets/shared/desktop/image-category-thumbnail-headphones.png"
+
+import earphones from "public/assets/shared/desktop/image-category-thumbnail-earphones.png"
+
 
 export const menuData = {
   logo: {
     image: audiophile,
-    imageAlt: "audiophile logo",
     link: "/",
   },
   menu: [
-    {
-      title: "home",
-      link: "/",
-      active: false,
-      cta: "shop",
-      image: {
-        src: headphones,
-        alt: "headphone",
-      },
-    },
+    
     {
       title: "headphones",
       link: "/headphones",
@@ -57,10 +50,7 @@ export const menuData = {
       "Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.",
     copyright: "Copyright 2021. All Rights Reserved",
   },
-  cart: {
-    icon: cart,
-    iconAlt: "cart icon notification",
-  },
+  
   socials: [
     {
       icon: "fab fa-facebook-square",
