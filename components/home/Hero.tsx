13dfe1dad9d6d80db629aa7eda4d2e108f5e212d
Israel Adefidipe <israeladefidipe@gmail.com>
2022-06-15 04:33:12 +0100

    feat: implemented home hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Wrapper } from "components/shared"
+import { BtnPry } from "components/shared/Button"
+import React from "react"
+import Image from "next/image"
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className='bg-hero-mobile sm:bg-hero-tablet lg:bg-hero-desktop bg-no-repeat bg-cover '>
+      <Wrapper>
+        <div className=' px-pad pt-[126px] pb-[167px]  lg:flex  '>
+          <div className='text-white flex-1  flex flex-col items-center text-center gap-[24px] lg:items-start lg:text-left '>
+            <h5 className='uppercase'>NEW PRODUCT</h5>
+            <h1 className='uppercase'>XX99 Mark II Headphones</h1>
+            <p>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <BtnPry className='btnTwo mt-[26px] ' />
+          </div>
+          <div className=' hidden flex-1 lg:block '></div>
+        </div>
+      </Wrapper>
+    </section>
   )
 }
 
