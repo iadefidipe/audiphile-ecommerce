32f7431a95bae3986688baa02d54861c9def47af
Israel Adefidipe <israeladefidipe@gmail.com>
2022-06-15 04:33:44 +0100

    refactor: fixed menu component

--- components/shared/MenuComponent.tsx
+++ components/shared/MenuComponent.tsx
@@ -3,28 +3,26 @@ import { Wrapper } from "components/shared"
 import Image from "next/image"
 import { BtnShop } from "./Button"
 import Link from "next/link"
-import { MenuInterface } from "utils/types"
 
-function menuComponent({ title, link, active, cta, image }: MenuInterface) {
+function menuComponent({ title, image, link }) {
   return (
-    <section className=' '>
-      <Wrapper>
-        <div className=' flex flex-col justify-end items-center bg-gray rounded-pry '>
+    <div className="  flex-1">
+      <Wrapper >
+        <div className=' mx-pad flex flex-col justify-end items-center bg-gray  rounded-pry '>
           <div className='category relative h-[150px] w-[230px] '>
-            <div className=' absolute top-[-50px]  '>
+            <div className=' absolute -translate-y-[70px]   '>
               <Image src={image.src} alt={image.alt} />
             </div>
           </div>
-
           <div className='flex flex-col gap-[15px] mb-[30px] '>
-            <h6 className=' font-bold uppercase  '>{title}</h6>
+            <h6 className=' font-bold uppercase '>{title}</h6>
             <Link href={link}>
               <BtnShop />
             </Link>
           </div>
         </div>
       </Wrapper>
-    </section>
+    </div>
   )
 }
 
