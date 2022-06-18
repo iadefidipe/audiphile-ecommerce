7d89dc509da6d09646b246023b13a8ad67f6c927
Israel Adefidipe <israeladefidipe@gmail.com>
2022-06-18 07:54:09 +0100

    feat: implemented mobile dropdown menu in header component

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,20 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 import Image from "next/image"
 import { Wrapper, MenuLinks, Logo, CartIcon } from "components/shared"
 import Hamburger from "public/assets/shared/tablet/icon-hamburger.svg"
+import MobileMenu from "./MobileMenu"
+import { menuData } from "data/menuData"
+
 
 function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <header className='bg-black py-8  left-0 right-0 z-50'>
       <Wrapper>
         <nav className=' nav '>
-          <div className=' md:hidden'>
+          <div className=' md:hidden' onClick={() => setToggleMenu(!toggleMenu)} >
             <Image src={Hamburger} alt='audiophile logo' />
           </div>
           <Logo />
-          <MenuLinks />
+          <MenuLinks  />
           <CartIcon />
         </nav>
+        { toggleMenu && <MobileMenu data={menuData.menu} />}
       </Wrapper>
     </header>
   )
