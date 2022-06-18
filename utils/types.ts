1ed10ddfd0f9f4ab67f5e9d498c687c277dec277
Israel Adefidipe <israeladefidipe@gmail.com>
2022-06-18 06:35:56 +0100

    refactor: add product data interface

--- utils/types.ts
+++ utils/types.ts
@@ -10,3 +10,44 @@ export interface MenuInterface {
     alt: string
   }
 }
+
+//TODO: Type => Product Data Types
+export interface ProductType {
+  id: number;
+  cartQuantity: number;
+  shortName: string;
+  cartIcon: string;
+  slug: string;
+  name: string;
+  subTotal: number;
+  image: ImagesTypes;
+  category: string;
+  categoryImage: ImagesTypes;
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: IncludesTypes[];
+  gallery: GalleryTypes;
+  others: OthersType[];
+}
+
+interface IncludesTypes {
+  quantity: number;
+  item: string;
+}
+interface GalleryTypes {
+  first: ImagesTypes;
+  second: ImagesTypes;
+  third: ImagesTypes;
+}
+interface OthersType {
+  slug: string;
+  name: string;
+  image: ImagesTypes;
+}
+interface ImagesTypes {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
