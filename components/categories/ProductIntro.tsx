d69b8ec73ae15eaff5bc33dde6cd82f9e1b9bf51
Israel <israeladefidipe@gmail.com>
2022-06-19 12:00:11 +0100

    feat: product component implemented

--- components/categories/ProductIntro.tsx
+++ components/categories/ProductIntro.tsx
@@ -1,19 +1,34 @@
 import { ImageContainer, Wrapper } from "components/shared"
 import React from "react"
 import { ProductType } from "utils/types"
-import Image from 'next/image'
+import { BtnPry } from "components/shared/Button"
 
 interface ProductIntroInterface {
   data: ProductType[]
 }
 
 function ProductIntro({ data }: ProductIntroInterface) {
-  
   return (
     <Wrapper>
       {data.map((product, index) => (
-        <section key={index}>
-          <ImageContainer desktopImage={product.image.desktop} tabletImage={product.image.tablet} mobileImage={product.image.mobile} />
+        <section
+          key={index}
+          className='mx-pad py-[120px] lg:py-[160px] flex flex-col items-center justify-center gap-[32px] lg:flex-row lg:odd:flex-row-reverse lg:gap-[125px] '
+        >
+          <div>
+            <ImageContainer
+              desktopImage={product.image.desktop}
+              tabletImage={product.image.tablet}
+              mobileImage={product.image.mobile}
+              category
+            />
+          </div>
+          <div className='flex-1 flex flex-col items-center gap-[24px] lg:items-start'>
+            <p className=" overline-pry " >NEW PRODUCT</p>
+            <h2 className="text-center lg:text-left uppercase" >{product.name}</h2>
+            <p className="text-center text-black opacity-50 font-medium lg:text-left " >{product.description}</p>
+            <BtnPry className='btnTwo' />
+          </div>
         </section>
       ))}
     </Wrapper>
